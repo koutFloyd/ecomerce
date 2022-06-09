55a1531bf3d62384459fec0e1f9bc433563d59a5
OTORRES <ostorresm@gmail.com>
2022-06-09 15:35:17 -0400

    subida 09/06/2022 15:35:15

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+  devise_for :admins
+  
+  authenticate :admin do
+    resources :products
+    resources :categories
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "home#index"
 end
