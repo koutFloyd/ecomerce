3fcfba8774680d10d0c87898cb30c1c3470da633
OTORRES <ostorresm@gmail.com>
2022-06-09 12:36:07 -0400

    subida 09/06/2022 12:35:59

---
+++ heroku.rb
@@ -0,0 +1,21 @@
+t = Time.now
+
+puts "running comandos gitHub..."
+puts "1.- running command git add ."
+system("git add .")
+puts "2.- running command git commit -m 'commit message'"
+system("git commit -m \"subida #{t.strftime("%d/%m/%Y %H:%M:%S")}\"")
+puts "3.- running command git push origin master"
+system("git push origin master")
+
+puts "running comandos DataBase..."
+system("db:migrate")
+
+puts "running comandos heroku..."
+puts "4.- running command heroku login"
+system("heroku login")
+system("horoku create")
+puts "5.- running command git push heroku master"
+system("git push heroku master")
+puts "6.- running command heroku run rake db:migrate"
+system("heroku run rake db:migrate")
